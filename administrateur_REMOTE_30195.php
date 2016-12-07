adbcc74724cfa41e96e45a6851272ad9fb14f761
Julienr85 <julienr85@gmail.com>
2016-12-07 08:48:31 +0100

    admin ok

--- administrateur_REMOTE_30195.php
+++ administrateur_REMOTE_30195.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:admin/administrateur.php
 <?php include 'header_admin.php'; ?>
 
 <div class="container">
@@ -44,49 +43,3 @@
 
 
 <?php include 'footer.php'; ?>
-=======
-<?php include 'header_admin.php'; ?>
-
-<div class="container">
-
-<div class="row">
-
-  <div class="col-md-12">
-          <div class="center-block text-center">
-              <h1>Bienvenue sur la page de gestion des Machines Virtuelles.</h1>
-              <p class="lead">Vous pouvez ajouter ou supprimer des machines virtuelles</p>
-          </div>
-  </div>
-</div><!--/row-->
-       <hr>
-<div class="row">
-              <section class="page1_header ">
-                    <div class="container">
-                      <div class="row">
-                        <div class="grid_5">
-                          <a href="ajout_VM.php" class="banner "><div class="maxheight">
-                            <div class="glyphicon glyphicon-download"></div></div>
-                            <div>Ajout VM</div>
-                          </a>
-                          <a href="produit_supprimer.php" class="banner "><div class="maxheight">
-                            <div class="glyphicon glyphicon-trash"></div></div>
-                            <div>Suppression VM</div>
-                          </a>
-                          <a href="" class="banner "><div class="maxheight1">
-                            <div class="glyphicon glyphicon-cog"></div></div>
-                            <div>Gestion Client</div>
-                          </a>
-                          <a href="produit.php" class="banner "><div class="maxheight1">
-                            <div class="glyphicon glyphicon-hdd"></div></div>
-                            <div>Produit</div>
-                          </a>
-                        </div>
-                      </div>
-                </section>
-            </div><!--/col-->
-</div><!--/row-->
-</div><!--/container-->
-
-
-<?php include 'footer.php'; ?>
->>>>>>> origin/Panier:administrateur.php
