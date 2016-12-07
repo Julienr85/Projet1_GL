c36daedab006ef550f28cbb30dac93c806f9081b
Julienr85 <julienr85@gmail.com>
2016-12-07 09:34:26 +0100

    Merge remote-tracking branch 'origin/produit_récup_panier' into produit_récup_panier
    # Conflicts:
    #	.DS_Store
    #	produit_ajout.php

--- authentification.php
+++ authentification.php
@@ -1,7 +1,5 @@
 <?php
 
-$monfichier = fopen('admin.txt', 'r');
-
 if (empty ( $_POST['inputUser']) && empty ( $_POST['inputPassword']))
 {
  echo "Vide";
@@ -14,18 +12,19 @@ else
  {
    $resultats = fgetcsv ( $monfichier , 200, ";");
 
-     if ($_POST['inputUser'] ==  $resultats[0]) {
+   if ($_POST['inputUser'] ==  $resultats[0]) {
 
-         if ($_POST['inputPassword'] ==  $resultats[1]) {
-             header('location:http://localhost:8888/Projet1_GL/administrateur.php');
-             break;
-           } else {
-           echo "Problème de mot de passe";
-          }
-     } else {
-         echo "Problème de user";
-     }
+       if ($_POST['inputPassword'] ==  $resultats[1]) {
+           header('location:http://localhost:8888/TP_PHP_ROUE/administrateur.php');
+           break;
+         } else {
+         echo "Problème de mot de passe";
+        }
+   } else {
+       echo "Problème de user";
+   }
  }
  fclose($monfichier);
 }
+
 ?>
