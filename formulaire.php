edb50fc4a4bcb0d0c4514aeea5d4e50d407e21d3
Julienr85 <julienr85@gmail.com>
2017-01-20 08:34:05 +0100

    dernière maj

--- formulaire.php
+++ formulaire.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+/* Changer la redirection pour la sauvegarde des identifiants vers la BDD*/
 $monfichier = fopen('formulaire.json', 'a');
 if ($monfichier){
   $string =  $_POST['nom'] . ";" .  $_POST['prenom']  . ";" . $_POST['inputEmail']. ";" . $_POST['inputPassword']. ";" . $_POST['reenterPassword']. ";" . "\n";
